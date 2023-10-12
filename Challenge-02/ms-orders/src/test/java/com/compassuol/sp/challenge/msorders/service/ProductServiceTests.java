ea3e94e2cc36261902326e6e1171355351eef269
Yasmim-bs <yasmimsabrinacp@gmail.com>
2023-10-12 19:32:26 -0300

    test: adding unit test of ProductService

---
+++ Challenge-02/ms-orders/src/test/java/com/compassuol/sp/challenge/msorders/service/ProductServiceTests.java
@@ -0,0 +1,39 @@
+package com.compassuol.sp.challenge.msorders.service;
+
+import com.compassuol.sp.challenge.msorders.dto.ProductDTO;
+import com.compassuol.sp.challenge.msorders.repository.ProductRepository;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import java.util.Collections;
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.mockito.Mockito.when;
+
+@ExtendWith(MockitoExtension.class)
+public class ProductServiceTests {
+
+    @InjectMocks
+    private ProductService productService;
+
+    @Mock
+    private ProductRepository productRepository;
+    ProductDTO productDTO;
+    @BeforeEach
+    void setUp(){
+        productDTO = new ProductDTO(1L," ",0.0," ");
+    }
+
+    @Test
+    public void listProducts_ReturnsNoProducts() {
+        when(productService.getAll()).thenReturn(Collections.emptyList());
+        List<ProductDTO> result = productService.getAll();
+        assertThat(result).isEmpty();
+    }
+
+}
