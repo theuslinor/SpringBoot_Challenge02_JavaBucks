ba8558ff1c016a366c547615e6d8e25abbfb35f2
Yasmim-bs <yasmimsabrinacp@gmail.com>
2023-10-13 21:13:33 -0300

    feat: API documentation on swagger

--- Challenge-02/ms-orders/src/main/java/com/compassuol/sp/challenge/msorders/controller/ProductController.java
+++ Challenge-02/ms-orders/src/main/java/com/compassuol/sp/challenge/msorders/controller/ProductController.java
@@ -1,25 +1,16 @@
 package com.compassuol.sp.challenge.msorders.controller;
 
 import com.compassuol.sp.challenge.msorders.dto.ProductDTO;
-import com.compassuol.sp.challenge.msorders.entity.Product;
-import com.compassuol.sp.challenge.msorders.repository.ProductRepository;
-import io.swagger.v3.oas.annotations.Operation;
-import io.swagger.v3.oas.annotations.tags.Tag;
+import com.compassuol.sp.challenge.msorders.service.ProductService;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
-import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import com.compassuol.sp.challenge.msorders.service.ProductService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+
 import java.util.List;
 
 
-@Tag(name="api-Product")
 @RequiredArgsConstructor
 @RestController
 @RequestMapping("/products")
