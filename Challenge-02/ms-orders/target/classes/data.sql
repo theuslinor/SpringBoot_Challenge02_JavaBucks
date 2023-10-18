31840b5c395b29fc01bc4741c5bca7e03e33414f
Yasmim-bs <yasmimsabrinacp@gmail.com>
2023-10-18 00:06:25 -0300

    finish

---
+++ Challenge-02/ms-orders/target/classes/data.sql
@@ -0,0 +1,13 @@
+INSERT INTO products_tb (id, name, value, description)
+VALUES
+
+ (1, 'The Witcher 3: Wild Hunt', 29.98, 'Embark on an epic monster hunting journey. Choices impact destiny in this immersive RPG.'),
+ (2, 'Overwatch 2', 0, 'Frantic battles between heroes in teams. Strategy and skill come together in this colorful shooter.'),
+ (3, 'Stardew Valley', 39.99, 'Farm your farm, fish and forge friendships in peaceful Pelican Town. A relaxing experience.'),
+ (4, 'Red Dead Redemption 2', 59.99, 'Experience the saga of outlaw Arthur Morgan in the Old West. Action, morality and a vast open world.'),
+ (5, 'Minecraft', 24.98, 'Build your pixelated world, explore and survive. Limitless creativity in this global phenomenon.'),
+ (6, 'Celeste', 9.99, 'Overcome challenges on a magical mountain. An engaging story of self-discovery.'),
+ (7, 'Assassin''s Creed Valhalla', 59.99, 'Lead Viking raids in medieval England. Combat, story and exploration intertwine.'),
+ (8, 'Rocket League', 0, 'Football meets speeding cars. Electrifying matches and multiplayer fun.'),
+ (9, 'Hollow Knight', 15.99, 'Explore a dark and mysterious world. Challenging combat and a captivating journey.'),
+ (10, 'Cyberpunk 2077', 44.99, 'Immerse yourself in Night City, a dystopian future. Choices impact the protagonist’s destiny.');
