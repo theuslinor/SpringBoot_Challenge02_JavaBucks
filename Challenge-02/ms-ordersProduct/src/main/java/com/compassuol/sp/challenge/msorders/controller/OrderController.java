bfcc00c49097daca23ac4803490e87f8770d4103
Yasmim-bs <yasmimsabrinacp@gmail.com>
2023-10-19 20:51:29 -0300

    .

--- Challenge-02/ms-ordersProduct/src/main/java/com/compassuol/sp/challenge/msorders/controller/OrderController.java
+++ Challenge-02/ms-ordersProduct/src/main/java/com/compassuol/sp/challenge/msorders/controller/OrderController.java
@@ -1,23 +1,41 @@
 package com.compassuol.sp.challenge.msorders.controller;
 
 
-import com.compassuol.sp.challenge.msorders.model.AddressClient.AddressClient;
-import com.compassuol.sp.challenge.msorders.model.AddressClient.response.services.AddressClient;
 import com.compassuol.sp.challenge.msorders.model.dto.OrderDTO;
+import com.compassuol.sp.challenge.msorders.model.entity.Order;
+import com.compassuol.sp.challenge.msorders.model.services.AddressClient;
 import com.compassuol.sp.challenge.msorders.service.OrderService;
-import jakarta.validation.Valid;
+import com.compassuol.sp.challenge.msorders.service.mapper.OrderMapper;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RequiredArgsConstructor
 @RestController
-@RequestMappOrderController {
+@RequestMapping("/orders")
+public class OrderController {
 
     private final OrderService orderService;
+
+    private final OrderMapper orderMapper;
 
     @GetMapping("/consulta")
     public ResponseEntity zipCodeQuery(@RequestBody AddressClient addressClient){
         return ResponseEntity.ok(orderService.searchCep(addressClient));
+    }
+
+    @GetMapping("/consultaOrder")
+    public ResponseEntity Order(@RequestBody OrderDTO orderRequest){
+        Order orderResponse = orderMapper.createOrderDTO(orderRequest);
+
+        return null;
+    }
+
+    @ResponseStatus(HttpStatus.NOT_FOUND)
+    @GetMapping("/{id}")
+    public ResponseEntity<OrderDTO> getOrdersById(@PathVariable Long id){
+        OrderDTO orderDTO= orderService.getOrderById(id);
+        return ResponseEntity.ok().body(orderDTO);
     }
 }
